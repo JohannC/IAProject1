4d488a0d483172782e733df0017be49e128ea5b2
JohannC <johann.corcuff@gmail.com>
2017-09-26 14:55:36 -0300

    Main is now recording time

--- Main.py
+++ Main.py
@@ -1,15 +1,40 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 from RandomCityMap import RandomCityMap
 from LocalSearch import LocalSearch
 from GeneticSearch import GeneticSearch
+from GeneticSearchLocalMinimum import GeneticSearchLocalMinimum
+
 
 cityMap1 = RandomCityMap(10)
-print(LocalSearch.search(cityMap1))
-print(GeneticSearch.search(cityMap1))
+startTime = datetime.datetime.now()
+path = LocalSearch.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Local Search 10 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+startTime = datetime.datetime.now()
+path = GeneticSearch.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Genetic Search 10 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+startTime = datetime.datetime.now()
+path = GeneticSearchLocalMinimum.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Genetic Search with Local minimum 10 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+
 
 cityMap1 = RandomCityMap(30)
-print(LocalSearch.search(cityMap1))
-print(GeneticSearch.search(cityMap1))
+startTime = datetime.datetime.now()
+path = LocalSearch.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Local Search 30 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+startTime = datetime.datetime.now()
+path = GeneticSearch.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Genetic Search 30 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+startTime = datetime.datetime.now()
+#path = GeneticSearchLocalMinimum.search(cityMap1)
+endTime = datetime.datetime.now()
+print('Genetic Search with Local minimum 30 cities : '+str(cityMap1.getTotalCost(path))+' in '+str(endTime - startTime))
+
 
 
